6889eef2db75c08f9fe0dec3adddd3fdb283e11e
liangxuejie <liangxuejie704@163.com>
2024-08-28 21:15:33 +0800

    Tab组件开发

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Header from '@/components/header/header.jsx'
+import Header from '@/components/Header/Header.jsx'
+import Tab from '@/components/Tab/Tab.jsx'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Tab></Tab>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/components/Tab/Tab.jsx
@@ -0,0 +1,41 @@
+import styles from './style.module.scss'
+import { NavLink } from "react-router-dom";
+import cn from 'classnames'
+
+const tabs = [
+  {
+    name: '推荐',
+    path: '/recommend'
+  },
+  {
+    name: '歌手',
+    path: '/singer'
+  },
+  {
+    name: '排行',
+    path: '/top-list'
+  },
+  {
+    name: '搜索',
+    path: '/search'
+  }
+]
+
+const Tab = () => {
+  return (
+    <div className={styles.tab}>
+      {tabs.map((tab) => (
+        <NavLink
+          className={({ isActive }) => isActive ? cn(styles.tabItem, styles.tabLinkActive) : styles.tabItem}
+          key={tab.path}
+          to={tab.path}>
+          <span className={styles.tabLink}>
+            {tab.name}
+          </span>
+        </NavLink>
+      ))}
+    </div>
+  )
+}
+
+export default Tab
